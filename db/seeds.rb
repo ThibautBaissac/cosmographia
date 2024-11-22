ed6e29d435606f0d89c87182a7a86bd4887ee9db
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-11-22 16:10:29 +0100

    Add image attachment to Map model and update views and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'faker'
 
+# Create 10 users
 10.times do
   User.create!(
     email: Faker::Internet.unique.email,
@@ -9,8 +10,9 @@ require 'faker'
 end
 puts "10 users created!"
 
+# Attach images to 100 maps
 100.times do
-  Map.create!(
+  map = Map.create!(
     user_id: User.pluck(:id).sample,
     name: Faker::Lorem.sentence(word_count: 3),
     description: Faker::Lorem.paragraph(sentence_count: 2),
@@ -21,5 +23,12 @@ puts "10 users created!"
     projection: [ "Mercator", "Lambert", "Orthographic", "Gnomonic" ].sample,
     coordinate_system: [ "WGS84", "NAD83", "ETRS89" ].sample
   )
+
+  image_path = Rails.root.join('db/seeds/images', "sample_#{rand(0..8)}.jpg")
+  map.image.attach(
+    io: File.open(image_path),
+    filename: "sample#{rand(1..5)}.jpg",
+    content_type: 'image/jpeg'
+  )
 end
-puts "100 maps created!"
+puts "100 maps created with images!"
