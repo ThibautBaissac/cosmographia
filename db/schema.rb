1954230525052b3b7fab98155b42e043ee4b6470
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-11-22 15:46:04 +0100

    Add Maps feature with index view, controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_11_22_140546) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_22_140954) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "maps", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.text "description"
+    t.date "creation_date"
+    t.integer "scale"
+    t.text "sources"
+    t.string "geographic_coverage"
+    t.string "projection"
+    t.string "coordinate_system"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_maps_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,4 +40,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_11_22_140546) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "maps", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+10.times do
+  User.create!(
+    email: Faker::Internet.unique.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+end
+puts "10 users created!"
+
+100.times do
+  Map.create!(
+    user_id: User.pluck(:id).sample,
+    name: Faker::Lorem.sentence(word_count: 3),
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    creation_date: Faker::Date.backward(days: 365 * 10),
+    scale: rand(1..10000),
+    sources: Faker::Lorem.sentence(word_count: 5),
+    geographic_coverage: Faker::Address.country,
+    projection: [ "Mercator", "Lambert", "Orthographic", "Gnomonic" ].sample,
+    coordinate_system: [ "WGS84", "NAD83", "ETRS89" ].sample
+  )
+end
+puts "100 maps created!"
