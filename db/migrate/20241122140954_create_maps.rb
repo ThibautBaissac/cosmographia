0777c8ac7bac489eacaa4452cabbad517f930310
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-11-25 14:16:14 +0100

    Run rubocop -a

--- db/migrate/20241122140954_create_maps.rb
+++ db/migrate/20241122140954_create_maps.rb
@@ -1,16 +1,16 @@
 class CreateMaps < ActiveRecord::Migration[8.0]
   def change
-    create_table :maps do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :title, null: false
-      t.text :description
-      t.date :creation_date
-      t.integer :scale
-      t.text :sources
-      t.string :geographic_coverage
-      t.string :projection
-      t.string :coordinate_system
-      t.boolean :is_public, default: true
+    create_table(:maps) do |t|
+      t.references(:user, null: false, foreign_key: true)
+      t.string(:title, null: false)
+      t.text(:description)
+      t.date(:creation_date)
+      t.integer(:scale)
+      t.text(:sources)
+      t.string(:geographic_coverage)
+      t.string(:projection)
+      t.string(:coordinate_system)
+      t.boolean(:is_public, default: true)
 
       t.timestamps
     end
--- db/migrate/20241124141233_add_superadmin_to_users.rb
+++ db/migrate/20241124141233_add_superadmin_to_users.rb
@@ -1,5 +1,5 @@
 class AddSuperadminToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :superadmin, :boolean, default: false
+    add_column(:users, :superadmin, :boolean, default: false)
   end
 end
