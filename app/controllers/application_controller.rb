0e11ef4b8f0dd8a2f861f978e2a64a694988e653
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-11-27 11:59:47 +0100

    Add locale management to application controller and update login view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,29 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   include Pagy::Backend
+
+  before_action :set_locale
+
+  private
+
+  def current_user_locale
+    current_user&.locale
+  end
+
+  def set_locale
+    if params[:locale] && !I18n.available_locales.map(&:to_s).include?(params[:locale])
+      redirect_to(url_for(locale: I18n.default_locale)) and return
+    end
+
+    I18n.locale = url_locale || current_user_locale || I18n.default_locale
+    session[:locale] = I18n.locale
+  end
+
+  def url_locale
+    params[:locale] if I18n.available_locales.map(&:to_s).include?(params[:locale])
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
