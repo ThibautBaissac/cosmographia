a852da1a2a005f9013a4ebbe33d3b8aff9ffa026
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-11-29 14:03:26 +0100

    Enhance home page for signed-in users with map display and layout adjustments

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,19 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @maps = random_maps_for_today
+    if user_signed_in?
+      @columns = distribute_maps_into_columns(3)
+    else
+      @maps = random_maps_for_today
+    end
   end
 
   private
+
+  def distribute_maps_into_columns(number_of_columns)
+    maps = Map.limit(10).includes(:image_attachment, :user)
+    maps.group_by.with_index { |_, index| index % number_of_columns }.values
+  end
 
   def random_maps_for_today
     seed = Digest::MD5.hexdigest(Date.today.to_s).to_i(16) # Generate a seed based on the current date
