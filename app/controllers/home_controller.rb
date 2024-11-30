ee38982a5d83fc2016b947c45ff1d9712c1104f5
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-11-30 16:24:46 +0100

    Add charts and last comments sections to home page for signed-in users

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,23 @@ class HomeController < ApplicationController
   def index
     if user_signed_in?
       @columns = Maps::ColumnDistributed.new(column_count: 3, map_count: 9).call
+      last_comments
+      set_charts
     else
       @maps = Maps::DailyRandom.new(map_count: 3).call
     end
+  end
+
+  private
+
+  def last_comments
+    @last_comments = Comment.includes(:user, :map).order(created_at: :desc).limit(5)
+  end
+
+  def set_charts
+    @maps_over_time = Map.group_by_month(:creation_date).count
+    @maps_by_projection = Map.group(:projection).count
+    @maps_by_coordinate_system = Map.group(:coordinate_system).count
+    @software_usage = MapSoftware.joins(:software).group("softwares.name").count
   end
 end
