764f4d0872bb2f92f2d0cd419c63c386d92b1ede
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-01 11:06:57 +0100

    Add category enum to Software model and update software CSV data

--- app/models/software.rb
+++ app/models/software.rb
@@ -3,4 +3,6 @@ class Software < ApplicationRecord
   has_many :maps, through: :map_softwares
 
   validates :name, :category, presence: true
+
+  string_enum category: %w[gis raster vector code images images_processing database other]
 end
