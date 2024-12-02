597a6837b93a9fbf14f73cf2925cf222dc83d597
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-02 16:39:26 +0100

    Add user_softwares association to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :maps, dependent: :destroy
   has_many :feedbacks, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :user_softwares, dependent: :destroy
+  has_many :softwares, through: :user_softwares
 
   validates :locale, presence: true, inclusion: {in: I18n.available_locales.map(&:to_s)}
 
