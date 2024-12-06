b8da67aaf4d56fe58962c3ec6180d4d65b536a6c
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-06 14:25:53 +0100

    Implement Pundit authorization for maps and add localized error messages

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,6 +2,7 @@ class MapsController < ApplicationController
   before_action :set_map, only: [ :edit, :update ]
 
   def index
+    authorize(Map)
     @query = params[:query]&.strip
     maps = Map.includes(:user, :softwares, :image_attachment).references(:softwares, :user)
 
@@ -14,6 +15,7 @@ class MapsController < ApplicationController
 
   def show
     @map = Map.find(params[:id])
+    set_authorize
     @maps = Map.order("RANDOM()").includes(:user, :image_attachment).limit(10)
     @comments = @map.comments.order(created_at: :desc)
     @new_comment = Comment.new
@@ -22,11 +24,12 @@ class MapsController < ApplicationController
 
   def new
     @map = Map.new
+    set_authorize
   end
 
   def create
-    @map = Map.new(map_params)
-    @map.user_id = current_user.id
+    @map = current_user.maps.new(map_params)
+    set_authorize
 
     if @map.save
       redirect_to(@map, notice: "Map was successfully created.")
@@ -36,9 +39,11 @@ class MapsController < ApplicationController
   end
 
   def edit
+    set_authorize
   end
 
   def update
+    set_authorize
     if @map.update(map_params)
       redirect_to(@map, notice: "Map was successfully updated.")
     else
@@ -52,10 +57,13 @@ class MapsController < ApplicationController
     @map = Map.find(params[:id])
   end
 
+  def set_authorize
+    authorize(@map)
+  end
+
   def map_params
     params.require(:map).permit(:title, :description, :creation_date, :scale, :sources, :geographic_coverage, :projection, :is_public, software_ids: [])
   end
-
 
   def map_filter_params
     params.permit(
---
+++ app/policies/map_policy.rb
@@ -0,0 +1,25 @@
+class MapPolicy < ApplicationPolicy
+  def index?
+    user.present?
+  end
+
+  def show?
+    user.present?
+  end
+
+  def new?
+    user&.not_guest? || user&.superadmin?
+  end
+
+  def create?
+    new?
+  end
+
+  def edit?
+    record.user == user || user&.superadmin?
+  end
+
+  def update?
+    edit
+  end
+end
