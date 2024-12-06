a873e9e3f2b7fa952635fd2911305dfcbfcde018
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-06 13:29:50 +0100

    Configure Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,10 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  include Pundit::Authorization
   include Pagy::Backend
 
   before_action :authenticate_user!
-
   before_action :set_locale
 
   private
