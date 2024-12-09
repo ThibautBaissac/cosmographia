6edaa1c3fb86ba0b3bd677f315f6242e1fbf8a46
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-09 13:41:32 +0100

    Add image format validation and update form to restrict file types

--- app/models/visualization.rb
+++ app/models/visualization.rb
@@ -7,12 +7,24 @@ class Visualization < ApplicationRecord
   has_many :softwares, through: :visualization_softwares
   has_one_attached :image
 
+
   validates :title, presence: true
   validates :image, presence: true
   validates :category, presence: true
   validates :description, presence: true
+  validate :image_format
 
   string_enum category: Constants::Visualizations::CATEGORY
   string_enum projection: Constants::Visualizations::PROJECTIONS
   string_enum geographic_coverage: Constants::Visualizations::GEOGRAPHIC_COVERAGE
+
+  private
+
+  def image_format
+    return unless image.attached?
+    return if image.blob.content_type.start_with?("image/")
+
+    image.purge
+    errors.add(:image, I18n.t("activerecord.errors.models.visualization.attributes.image.invalid_format"))
+  end
 end
