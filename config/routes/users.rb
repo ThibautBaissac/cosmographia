9215596abbf81ad178e7bd437952cdb72e70b9e6
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-12 11:45:02 +0100

    Add user profile route with slug constraint for locale handling

--- config/routes/users.rb
+++ config/routes/users.rb
@@ -1,1 +1,4 @@
 patch "update_user_locale", to: "users/locale#update"
+get "/:slug", to: "users#show", as: :user_profile, constraints: lambda { |req|
+  !I18n.available_locales.include?(req.params[:slug]&.to_sym)
+}
