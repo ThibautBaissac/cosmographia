a315f53a1c3c0888e6a2466561221759f2922e2d
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-14 17:08:57 +0100

    Implement header component in all views

--- app/helpers/back_button_helper.rb
+++
@@ -1,9 +0,0 @@
-module BackButtonHelper
-  def previous_page_link(path)
-    if request.path.start_with?(path) && request.referer.present?
-      request.referer
-    else
-      path || root_path
-    end
-  end
-end
