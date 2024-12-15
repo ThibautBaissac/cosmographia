02d5a5ccbb0a330397f3d345b067f3cd0883c8d7
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-15 15:11:40 +0100

    Fix pwa_manifest route to avoid conflict with user#slug

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   scope "(:locale)", locale: /#{I18n.available_locales.join('|')}/ do
+    # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
+    get "manifest" => "rails/pwa#manifest", as: :pwa_manifest # keep this route above the users routes to avoid conflicts between /user#slug and /manifest
+    # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+
     devise_for :users
 
     draw :community
@@ -15,10 +19,6 @@ Rails.application.routes.draw do
     # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
     # Can be used by load balancers and uptime monitors to verify that the app is live.
     get "up" => "rails/health#show", as: :rails_health_check
-
-    # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-    get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-    # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
     match "/404", to: "errors#not_found", via: :all
     match "/500", to: "errors#internal_server_error", via: :all
