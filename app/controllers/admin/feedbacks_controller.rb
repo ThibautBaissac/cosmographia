7aba9c1d5ee68990cb8ecb86458a12475fe1231d
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-15 10:32:46 +0100

    Fix typo in authorization method in FeedbacksController index action because

--- app/controllers/admin/feedbacks_controller.rb
+++ app/controllers/admin/feedbacks_controller.rb
@@ -1,6 +1,6 @@
 class Admin::FeedbacksController < ApplicationController
   def index
-    authorize([ :admin, :feedback ], :index)
+    authorize([ :admin, :feedback ], :index?)
     feedbacks = Feedback.includes(:user).order(created_at: :desc)
     @pagy, @feedbacks = pagy(feedbacks)
   end
