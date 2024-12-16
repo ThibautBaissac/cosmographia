5400f393c3efdf505431c0e421e3137fc91e263d
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-16 16:24:04 +0100

    Add factories for challenges and user challenges

---
+++ spec/factories/challenges.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :challenge do
+    title { "MyString" }
+    description { "MyText" }
+    start_date { "2024-12-16" }
+    end_date { "2024-12-16" }
+    difficulty { "MyString" }
+    category { "MyString" }
+  end
+end
---
+++ spec/factories/user_challenges.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user_challenge do
+    user { nil }
+    challenge { nil }
+  end
+end
