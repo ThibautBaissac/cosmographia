92121615ec017a84159c9648d1bd8fba646b7c3f
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-16 17:26:18 +0100

    Enhance challenge card display with status icons

--- db/seeds/data/challenges.rb
+++ db/seeds/data/challenges.rb
@@ -1,10 +1,12 @@
 puts("---- Creating challenges...")
 (0...@nb_challenges).each do |i|
+  start_date = Faker::Date.between(from: 1.month.ago, to: 1.month.from_now)
+  end_date = Faker::Date.between(from: start_date, to: start_date + 10.days)
   challenge = Challenge.create!(
     title: Faker::Lorem.sentence(word_count: 3),
     description: Faker::Lorem.sentence(word_count: 10),
-    start_date: Faker::Date.between(from: Date.tomorrow, to: 1.month.from_now),
-    end_date: Faker::Date.between(from: 1.month.from_now, to: 2.month.from_now),
+    start_date:,
+    end_date:,
     difficulty: Challenge.difficulty_values.sample,
     category: Challenge.category_values.sample
   )
