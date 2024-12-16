47a46e482fa67b2997c38deaa22895a83ea0146b
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-16 11:22:14 +0100

    Add feedback subjects as constants and implement string_enum validation

---
+++ app/classes/constants/feedbacks.rb
@@ -0,0 +1,5 @@
+module Constants
+  module Feedbacks
+    SUBJECTS = %w[feature_request bug_report general_feedback usability_issues account_management other].freeze
+  end
+end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,6 @@ class Feedback < ApplicationRecord
   belongs_to :user
 
   validates :subject, :message, presence: true
+
+  string_enum subject: Constants::Feedbacks::SUBJECTS
 end
