91dc5b9961d5813426aa70f4f578fc3fb1bf14d0
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-16 11:22:44 +0100

    Refactor feedback form to use select for subject field

--- db/seeds/data/feedbacks.rb
+++ db/seeds/data/feedbacks.rb
@@ -3,8 +3,8 @@ nb_feedbacks = 20
 (0...20).each do |i|
   Feedback.create!(
     user_id: User.pluck(:id).sample,
-    subject: Faker::Lorem.sentence,
-    message: Faker::Lorem.paragraph
+    subject: Feedback.subject_values.sample,
+    message: Faker::Lorem.paragraph(sentence_count: 5, supplemental: true, random_sentences_to_add: 10),
   )
   puts("--#{i + 1}/#{nb_feedbacks} feedback created!")
 end
