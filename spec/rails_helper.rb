0d2d17386089f9c82942192d01d1a0182235d4e0
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-17 09:19:38 +0100

    Add Capybara require in rails_helper.rb

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 return unless Rails.env.test?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'capybara/rails'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
