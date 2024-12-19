2f657d78d82442b063ba922d0079342942b0e313
ThibautBaissac <59996803+ThibautBaissac@users.noreply.github.com>
2024-12-19 11:55:33 +0100

    Add visualization selection and submission functionality to attach existing visualization to a challenge

--- app/controllers/challenges/attach_visualization_controller.rb
+++ app/controllers/challenges/attach_visualization_controller.rb
@@ -3,9 +3,13 @@ class Challenges::AttachVisualizationController < ApplicationController
   # before_action :set_authorize
 
   def new
+    @visualizations = current_user.visualizations.where(challenge_id: nil).includes(:image_attachment)
   end
 
   def create
+    visualization = current_user.visualizations.find(params[:visualization_id])
+    visualization.update(challenge_id: @challenge.id)
+    redirect_to(challenge_path(@challenge))
   end
 
   private
