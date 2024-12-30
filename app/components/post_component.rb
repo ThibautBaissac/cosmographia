13e4f2b2a0d70a90f9fa60c120dc31c2cff3ed3d
Thibaut Baissac <tbaissac@proton.me>
2024-12-30 14:23:18 +0100

    Create PostComponent for rendering visualizations/comment and challenge/discussion

---
+++ app/components/post_component.rb
@@ -0,0 +1,39 @@
+class PostComponent < ViewComponent::Base
+  include Turbo::FramesHelper
+
+  attr_reader :post, :edit_path, :destroy_partial, :locale, :current_user
+
+  def initialize(post:, edit_path:, destroy_partial:, locale:, current_user:)
+    @post = post
+    @edit_path = edit_path
+    @destroy_partial = destroy_partial
+    @locale = locale
+    @current_user = current_user
+  end
+
+  def editable?
+    policy(post).edit?
+  end
+
+  def user_fullname
+    post.user.fullname
+  end
+
+  def user_slug
+    post.user.slug
+  end
+
+  def timestamp
+    helpers.humanized_time_ago(post.created_at)
+  end
+
+  def post_content
+    simple_format(ContentPresenter.new(post.content, current_user).with_mentions)
+  end
+
+  private
+
+  def policy(record)
+    Pundit.policy(current_user, record)
+  end
+end
