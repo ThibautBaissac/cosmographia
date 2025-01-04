ca77b33b52439c164c01adaad30e636aae0da761
Thibaut Baissac <tbaissac@proton.me>
2025-01-04 15:18:41 +0100

    Add RSpec tests for ChallengesController index action with authentication scenarios

---
+++ spec/controllers/challenges/index_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe ChallengesController, type: :controller do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+  end
+
+  describe 'GET #index' do
+    context 'when user is authorized' do
+      before do
+        allow(controller).to receive(:authorize).and_return(true)
+        @challenges = create_list(:challenge, 5, start_date: Date.today)
+      end
+
+      it 'responds successfully with HTTP 200 status' do
+        get :index
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when user is unauthenticated' do
+      before do
+        sign_out user
+      end
+
+      it 'redirects to the sign-in page' do
+        get :index, params: { locale: 'en' }
+        expect(response).to redirect_to(new_user_session_path(locale: :en))
+      end
+    end
+  end
+end
