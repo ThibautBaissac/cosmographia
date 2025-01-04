7841117ecc91bc054c830f29239fc5f0a081bd1a
Thibaut Baissac <tbaissac@proton.me>
2025-01-04 15:42:19 +0100

    Remove redundant tests for visitors and guests in ChallengePolicy specs

--- spec/policies/challenge_policy_spec.rb
+++ spec/policies/challenge_policy_spec.rb
@@ -4,16 +4,6 @@ RSpec.describe ChallengePolicy do
   subject { described_class.new(user, challenge) }
   let(:challenge) { build(:challenge) }
 
-  context 'with visitors' do
-    let(:user) { nil }
-    it { is_expected.to forbid_all_actions }
-  end
-
-  context 'with guests' do
-    let(:user) { build(:user, :guest) }
-    it { is_expected.to forbid_all_actions }
-  end
-
   context 'with opted in users' do
     let(:user) { build(:user, :optin_directory) }
     it { is_expected.to permit_only_actions(%i[index participations discussion]) }
