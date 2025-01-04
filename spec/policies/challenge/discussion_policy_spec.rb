ad58e34e599b6b66db09c9f9c9c1bf5b735f896e
Thibaut Baissac <tbaissac@proton.me>
2025-01-04 16:45:58 +0100

    Add tests for Challenge::DiscussionPolicy

---
+++ spec/policies/challenge/discussion_policy_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Challenge::DiscussionPolicy do
+  let(:challenge) { create(:challenge, :with_users) }
+  let(:user) { challenge.users.first }
+  let(:superadmin) { create(:user, :superadmin) }
+  let(:challenge_discussion) { create(:challenge_discussion, challenge: challenge, user: user) }
+
+  context 'when the user is part of the challenge' do
+    subject { described_class.new(user, challenge_discussion) }
+    it { is_expected.to permit_only_actions(%i[new create edit update destroy]) }
+  end
+
+  context 'when the user is a superadmin' do
+    subject { described_class.new(superadmin, challenge_discussion) }
+    it { is_expected.to permit_only_actions(%i[edit update destroy]) }
+  end
+end
