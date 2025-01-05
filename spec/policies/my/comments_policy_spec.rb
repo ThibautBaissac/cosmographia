2fce603dcb0a745ccad9e984e18219c4a0201b96
Thibaut Baissac <tbaissac@proton.me>
2025-01-05 10:32:59 +0100

    Add RSpec tests for My::CommentsPolicy

---
+++ spec/policies/my/comments_policy_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe My::CommentsPolicy do
+  let(:record) { build(:user) }
+
+
+  context 'with the user being the same as the record' do
+    let(:user) { record }
+    subject { described_class.new(user, record) }
+
+    it { is_expected.to permit_only_actions(%i[show]) }
+  end
+end
