06ce0b1df71beb97c9bd8803005cf77e4a004a5c
Thibaut Baissac <tbaissac@proton.me>
2025-01-05 15:02:04 +0100

    Add RSpec tests for FeedbacksController#create action

---
+++ spec/controllers/feedbacks_controller_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe FeedbacksController, type: :controller do
+  let(:user) { create(:user) }
+  let(:valid_attributes) { attributes_for(:feedback) }
+  let(:invalid_attributes) { { subject: nil, message: nil } }
+
+  before do
+    sign_in user
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new feedback and redirects to root path" do
+        allow_any_instance_of(FeedbackPolicy).to receive(:create?).and_return(true)
+
+        expect {
+          post :create, params: { feedback: valid_attributes }
+        }.to change(Feedback, :count).by(1)
+
+        expect(response).to redirect_to(root_path(I18n.locale))
+      end
+    end
+
+    context "with invalid params" do
+      it "does not create a feedback and redirects to root path" do
+        allow_any_instance_of(FeedbackPolicy).to receive(:create?).and_return(true)
+
+        expect {
+          post :create, params: { feedback: invalid_attributes }
+        }.not_to change(Feedback, :count)
+
+        expect(response).to redirect_to(root_path(I18n.locale))
+      end
+    end
+
+    context "when unauthorized" do
+      it "does not create feedback and redirects" do
+        allow_any_instance_of(FeedbackPolicy).to receive(:create?).and_return(false)
+
+        expect {
+          post :create, params: { feedback: valid_attributes }
+        }.not_to change(Feedback, :count)
+
+        expect(response).to redirect_to(root_path(I18n.locale))
+      end
+    end
+  end
+end
--- spec/factories/feedback.rb
+++ spec/factories/feedback.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :feedback do
     association :user
+    subject { Feedback.subject_values.sample }
     message { "This is a sample message." }
   end
 end
