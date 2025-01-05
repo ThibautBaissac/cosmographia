68af454f97442dd7ca219df85932e4c8b9430fdf
Thibaut Baissac <tbaissac@proton.me>
2025-01-05 11:28:53 +0100

    Add RSpec tests for VisualizationsController#index

---
+++ spec/controllers/visualizations/index_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe VisualizationsController, type: :controller do
+  let(:user) { create(:user) }
+  before do
+    sign_in user
+  end
+
+  describe "GET #index" do
+    context "when user is authorized" do
+      before do
+        allow(controller).to receive(:authorize).and_return(true)
+        @visualizations = create_list(:visualization, 5)
+      end
+
+      it "responds successfully with HTTP 200 status" do
+        get :index
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "when user is unauthorized" do
+      before do
+        allow(controller).to receive(:authorize).and_raise(Pundit::NotAuthorizedError)
+      end
+
+      it "returns a redirect status" do
+        get :index
+        expect(response).to have_http_status(:found) # 302
+      end
+
+      it "redirects to the homepage page" do
+        get :index
+        expect(response).to redirect_to(root_path(locale: :en))
+      end
+    end
+
+    context "when user is unauthenticated" do
+      before do
+        sign_out user
+      end
+
+      it "redirects to the sign-in page" do
+        get :index, params: { locale: "en" }
+        expect(response).to redirect_to(new_user_session_path(locale: :en))
+      end
+    end
+  end
+end
