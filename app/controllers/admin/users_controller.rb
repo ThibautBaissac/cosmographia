4f9155631df8c56f52f080f8aeebd35b28582404
Thibaut Baissac <tbaissac@proton.me>
2025-01-20 11:44:33 +0100

    Add user management functionality with impersonation support for superadmins

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,19 @@
+class Admin::UsersController < ApplicationController
+  before_action :require_superadmin!
+
+  def index
+    users = User.order(:id)
+    @pagy, @users = pagy(users)
+  end
+
+  def impersonate
+    user = User.find(params[:id])
+    impersonate_user(user)
+    redirect_to root_path
+  end
+
+  def stop_impersonating
+    stop_impersonating_user
+    redirect_to root_path
+  end
+end
