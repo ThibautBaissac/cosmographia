ac8993cddbf2d714fe9bbc0a12ae275bca765529
Thibaut Baissac <tbaissac@proton.me>
2025-01-21 16:03:54 +0100

    Configure Active Storage to use Amazon S3 in production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # config.asset_host = "http://assets.example.com"
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Configure Active Storage to use a job queue.
   config.active_storage.queues.variant = :default
