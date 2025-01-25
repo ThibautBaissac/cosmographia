4f2a62b0d3bca7cd64344b63eeb687ed9883b22e
Thibaut Baissac <tbaissac@proton.me>
2025-01-25 14:19:45 +0100

    Refactor slug generation logic to improve readability and ensure uniqueness of slugs

--- app/models/concerns/user/slug_generation.rb
+++ app/models/concerns/user/slug_generation.rb
@@ -8,21 +8,29 @@ module User::SlugGeneration
   end
 
   def generate_slug
-    base_slug = if fullname.present?
-                  fullname.downcase.parameterize
+    base_slug = determine_base_slug
+    self.slug = unique_slug(base_slug)
+  end
+
+  private
+
+  def determine_base_slug
+    if fullname.present?
+      fullname.downcase.parameterize
     else
-                  email.split("@").first.parameterize
+      email.split("@").first.parameterize
+    end
+  end
+
+  def unique_slug(base_slug)
+    slug_candidate = base_slug
+    counter = 1
+
+    while ::User.where(slug: slug_candidate).where.not(id: id).exists?
+      slug_candidate = "#{base_slug}-#{counter}"
+      counter += 1
     end
 
-    self.slug = base_slug
-    existing_slugs = ::User.where.not(id: id).pluck(:slug)
-
-    if existing_slugs.include?(base_slug)
-      counter = 1
-      while ::User.exists?(slug: "#{base_slug}-#{counter}")
-        counter += 1
-      end
-      self.slug = "#{base_slug}-#{counter}"
-    end
+    slug_candidate
   end
 end
