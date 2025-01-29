9abc88c9c32eb3be02b46792ce1c476361397ec4
Thibaut Baissac <tbaissac@proton.me>
2025-01-29 16:03:54 +0100

    Add visualization form filling functionality to create visualization feature spec

--- spec/features/create_visualization.rb
+++ spec/features/create_visualization.rb
@@ -5,6 +5,7 @@ RSpec.feature("Create Visualization", type: :feature) do
   include BaseFeatureHelper
 
   let(:user) { create(:user, :with_free_subscription) }
+  let(:visualization_title) { "MyDataVisualization" }
 
   before do
     sign_in_as(email: user.email, password: user.password)
@@ -17,6 +18,22 @@ RSpec.feature("Create Visualization", type: :feature) do
 
       click_on("create_visualization_title")
       expect(page).to(have_selector("#new_visualization_header"))
+
+      fill_in_visualization_form
+      expect(page).to(have_content(visualization_title))
+    end
+  end
+
+  private
+
+  def fill_in_visualization_form
+    within("#visualization_form") do
+      choose("visualization_category_data")
+      fill_in("visualization[title]", with: visualization_title)
+      fill_in("visualization[creation_date]", with: 1.day.ago.strftime("%d/%m/%Y"))
+      fill_in("visualization[description]", with: "Description")
+      attach_file("visualization[image]", Rails.root.join("spec/fixtures/sample_1.jpg"))
+      click_on("commit")
     end
   end
 end
