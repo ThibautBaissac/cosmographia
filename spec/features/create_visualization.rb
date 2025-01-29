1792158eab280064ac53bdb2383381722f190b83
Thibaut Baissac <tbaissac@proton.me>
2025-01-29 16:09:26 +0100

    Add comment form filling functionality to visualization creation feature spec

--- spec/features/create_visualization.rb
+++ spec/features/create_visualization.rb
@@ -6,6 +6,7 @@ RSpec.feature("Create Visualization", type: :feature) do
 
   let(:user) { create(:user, :with_free_subscription) }
   let(:visualization_title) { "MyDataVisualization" }
+  let(:comment_content) { "MyNewComment" }
 
   before do
     sign_in_as(email: user.email, password: user.password)
@@ -21,6 +22,9 @@ RSpec.feature("Create Visualization", type: :feature) do
 
       fill_in_visualization_form
       expect(page).to(have_content(visualization_title))
+
+      fill_in_comment_form
+      expect(page).to(have_content(comment_content))
     end
   end
 
@@ -33,6 +37,13 @@ RSpec.feature("Create Visualization", type: :feature) do
       fill_in("visualization[creation_date]", with: 1.day.ago.strftime("%d/%m/%Y"))
       fill_in("visualization[description]", with: "Description")
       attach_file("visualization[image]", Rails.root.join("spec/fixtures/sample_1.jpg"))
+      click_on("commit")
+    end
+  end
+
+  def fill_in_comment_form
+    within("#new_comment_form") do
+      fill_in("visualization_comment[content]", with: comment_content)
       click_on("commit")
     end
   end
