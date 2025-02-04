ea5824e0b05ce8d2bf90f5593a0eafff376bba0d
Thibaut Baissac <tbaissac@proton.me>
2025-02-04 11:59:48 +0100

    Refactor users controller to improve method naming and enhance user data loading logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,33 +4,34 @@ class UsersController < ApplicationController
 
   def show
     authorize(@user)
-    set_visualizations
-    set_contributions
+    load_visualizations
+    load_contributions
     @pagy, @visualizations = pagy(@visualizations)
-  rescue Pundit::NotAuthorizedError
-    redirect_back(fallback_location: root_path(locale), alert: t("user.not_allowed")) and return
   end
 
   private
 
   def find_user
     @user = User.includes(user_softwares: :software).find_by(slug: params[:slug])
-    if @user.nil?
-      redirect_back(fallback_location: root_path(locale), alert: t("user.not_found")) and return
-    end
+    return if @user.present?
+
+    redirect_back(fallback_location: root_path(locale), alert: t("user.not_found"))
   end
 
   def user_public_profile?
     authorize(@user)
   end
 
-  def set_visualizations
+  def load_visualizations
     @visualizations = @user.visualizations.includes(:image_attachment)
   end
 
-  def set_contributions
-    contribution_service = UserContributions::ContributionsService.new(current_user, range: 1.year.ago..Time.current)
-    @contributions_by_day = contribution_service.contributions_by_day
-    @weeks = contribution_service.weeks
+  def load_contributions
+    contributions_service = UserContributions::ContributionsService.new(
+      @user,
+      range: 1.year.ago..Time.current
+    )
+    @contributions_by_day = contributions_service.contributions_by_day
+    @weeks = contributions_service.weeks
   end
 end
