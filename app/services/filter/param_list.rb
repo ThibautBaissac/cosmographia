d4f0d01072874133c3b31ab5a567c1441adca548
Thibaut Baissac <tbaissac@proton.me>
2025-02-04 10:52:29 +0100

    Refactor Filter::ParamList to improve clarity by converting params to a hashand freeze it in the initializer and by introducing dedicated methods

--- app/services/filter/param_list.rb
+++ app/services/filter/param_list.rb
@@ -1,9 +1,19 @@
 class Filter::ParamList
   def initialize(params:)
-    @params = params
+    @params = params.to_h.freeze
   end
 
   def non_empty
-    @params.to_h.reject { |_, v| v.blank? || (v.is_a?(Array) && v.all?(&:blank?)) }
+    filter_blank_values(@params)
+  end
+
+  private
+
+  def filter_blank_values(params)
+    params.reject { |_, v| blank_value?(v) }
+  end
+
+  def blank_value?(value)
+    value.blank? || (value.is_a?(Array) && value.all?(&:blank?))
   end
 end
