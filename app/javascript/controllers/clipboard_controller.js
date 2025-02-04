a6a0503363ec97f7d2960259207f12740592b032
Thibaut Baissac <tbaissac@proton.me>
2025-02-04 11:09:12 +0100

    Refactor clipboard stimulus controller to separation concerns and improve asynchronous operation

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,21 +3,34 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="clipboard"
 export default class extends Controller {
   static targets = ['text', 'message']
+  timeoutId = null // Reference to the timeout, so it can be cleared if needed
 
-  copy() {
-    navigator.clipboard.writeText(this.textTarget.textContent.trim())
-      .then(() => {
-        // Show the success message
-        this.messageTarget.textContent = 'Copied!'
-        this.messageTarget.classList.remove('d-none')
+  async copy() {
+    try {
+      const textToCopy = this.textTarget.textContent.trim()
+      await navigator.clipboard.writeText(textToCopy)
+      this.showMessage('Copied!')
+    } catch (error) {
+      console.error('Failed to copy text: ', error)
+    }
+  }
 
-        // Hide the success message after 2 seconds
-        setTimeout(() => {
-          this.messageTarget.classList.add('d-none')
-        }, 2000)
-      })
-      .catch((error) => {
-        console.error('Failed to copy text: ', error)
-      })
+  showMessage(message) {
+    this.messageTarget.textContent = message
+    this.messageTarget.classList.remove('d-none')
+
+    // Clear any existing timeout to avoid conflicts
+    if (this.timeoutId) {
+      clearTimeout(this.timeoutId)
+    }
+
+    // Hide the message after 2 seconds
+    this.timeoutId = setTimeout(() => {
+      this.hideMessage()
+    }, 2000)
+  }
+
+  hideMessage() {
+    this.messageTarget.classList.add('d-none')
   }
 }
